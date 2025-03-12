0421e310329142e971e2ea3a189a4deb163385d5
gamrodrigues <gabrielaugustomartinsrodrigues@gmail.com>
2025-03-11 21:34:30 -0300

    Arquivos Retroativos

---
+++ AULA_01.py
@@ -0,0 +1,17 @@
+print('boas vindas!')
+print("boas vindas!")
+
+var_1=input('informe seu nome: ')
+var_2=input('informe sua idade: ')
+print(var_1, ' tem ', var_2, ' anos')
+print('{1} tem {0} anos'.format(var_1, var_2))
+print(f'{var_1} tem {var_} anos')
+
+
+
+
+
+
+
+
+
